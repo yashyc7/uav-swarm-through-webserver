8a50114b7e99c898bc612f218bacad65f0fc278f
Yash Chauhan <yashc9411407582@gmail.com>
2025-06-29 19:53:32 +0530

    setup the sitl instance running in the windows and setup according to my system

--- slave2.py
+++ slave2.py
@@ -1,24 +1,26 @@
-#=============================slaveX.py===================================================================
+# =============================slaveX.py===================================================================
 # Author: Martin Pozniak
 # Desc: This script controls the behavior of the slave drone in flight.
 # Creation Date: 12/~/2017
 # Latest Functionality: Script is used to takeoff the slave and follow the master's movements
-#============================================================================================================= 
+# =============================================================================================================
 from droneBrain import Drone
 import time
-#import netifaces as ni
+# import netifaces as ni
 
-ipAddr = "192.168.1.3" # This is set in case you are on windows and the following code can't get the ip dynamically
-#============This Can Only Be Done On Linux!!==================
+ipAddr = "192.168.1.3"  # This is set in case you are on windows and the following code can't get the ip dynamically
+# ============This Can Only Be Done On Linux!!==================
 # ni.ifaddresses('wlan0')
 # ipAddr = ni.ifaddresses('waln0')[ni.AF_INET][0]['addr']
-#==============================================================
+# ==============================================================
 
-#====Set Arg To True For Sitl on Port=====
-#====Set To False For Physical Drone======
+# ====Set Arg To True For Sitl on Port=====
+# ====Set To False For Physical Drone======
 print("=====================SLAVE 2 : ID 3=======================")
-print("==================="+ipAddr+"===================")
-drone = Drone(useSitl=False,port="5790",ID='3',ip=ipAddr) #make sure IP specified here matches IP of the device
+print("===================" + ipAddr + "===================")
+drone = Drone(
+    useSitl=True, port="5790", ID="3", ip=ipAddr
+)  # make sure IP specified here matches IP of the device
 
 drone.set_airspeed(1)
 
@@ -29,16 +31,18 @@ print("===================================")
 print("Swarm:", drone.get_swarm_data())
 print("===================================")
 
-#===============MISSION====================
+# ===============MISSION====================
 
-#drone.arm_formation()
+# drone.arm_formation()
 
 print("===========WAITING_FOR_MASTER=============")
-drone.wait_for_drone_reach_altitude(droneID='1',altitude=2)
+drone.wait_for_drone_reach_altitude(droneID="1", altitude=2)
 print("=========================================")
 
 print("===========TAKING_OFF====================")
-drone.arm_and_takeoff(float(drone.get_data_from_server("/dronedata", {'droneID':'1'})['altitude']))
+drone.arm_and_takeoff(
+    float(drone.get_data_from_server("/dronedata", {"droneID": "1"})["altitude"])
+)
 print("=========================================")
 
 print("=======FOLLOWING MASTERS MOVEMENTS=======")
@@ -53,4 +57,4 @@ print("===========SHUTTING_DOWN=================")
 drone.shutdown()
 print("=========================================")
 
-#==========================================
+# ==========================================
